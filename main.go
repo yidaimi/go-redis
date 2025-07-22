436881dd47152cb35d6b219448bf232afc869677
yidaimi <yidaimi2013@163.com>
2025-07-22 11:03:33 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "github.com/redis/go-redis/v9"
+)
+
+func main() {
+    ctx := context.Background()
+    rdb := redis.NewClient(&redis.Options{
+        Addr: "localhost:6379",
+    })
+
+    err := rdb.Set(ctx, "key", "value", 0).Err()
+    if err != nil {
+        panic(err)
+    }
+
+    val, err := rdb.Get(ctx, "key").Result()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("key:", val)
+}
